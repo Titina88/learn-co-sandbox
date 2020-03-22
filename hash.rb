bb72dd43c4316e6dadb26744e8592a6587d1fa7f
Christina Placide <christina.placide@hotmail.com>
2020-03-22 21:32:46 +0000

    Automatically backed up by Learn

---
+++ hash.rb
@@ -0,0 +1,6 @@
+english_bands_by_city = {
+  :liverpool => "The Beatles",
+  :manchester => "The Smiths",
+  :conventry => "Delia Derbyshire and the BBC Radiophonic Band",
+  :london => "Ziggy arsStarburst and the Spiders from M"
+}
